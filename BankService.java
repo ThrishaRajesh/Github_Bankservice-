306ebc27bbe12c84509509c92684b10417fe002f
Thrisha Rajesh <23cse095@bnmit.in>
2026-01-29 22:21:05 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -10,7 +10,7 @@ class BankAccount
 		if(amount>0)
 		{
 			balance += amount;
-			System.out.println("Balance after deposit of 500:  "+balance);
+			System.out.println("Balance after deposit of 700:  "+balance);
 		}
 	}
 	void withdraw(double amount)
@@ -29,7 +29,7 @@ class BankAccount
 			BankAccount acc = new BankAccount(5000);
 			System.out.println("Initial balance: "+acc.balance);
 		
-			acc.deposit(500);
+			acc.deposit(700);
 			acc.withdraw(100);
 			
 			System.out.println("Final Balance: "acc.balance");
